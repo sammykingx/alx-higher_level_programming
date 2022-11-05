596abb985546923975624e4b6c991a07d3718ea9
sammykingx <hellosammy@sammykingx.com.ng>
2022-11-05 08:45:52 +0000

    a quick search nd replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+    '''
+        A function that traverse through a list for an element that matches
+        search and modify it with replace then returns a new list.
+
+    '''
+    if len(my_list) == 0:
+        return my_list
+
+    new_lst = [element if element != search else replace for element in my_list]
+    return new_lst
+
+
+# Test
+my_lst = [1, 2, 3, 4, 5, 7, 8]
+print(my_lst)
+print(search_replace(my_lst, 2, 89))
+
