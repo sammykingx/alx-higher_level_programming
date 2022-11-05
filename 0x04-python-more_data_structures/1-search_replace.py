45e40c54fa85344da71a679c364f7c091ad619b7
sammykingx <hellosammy@sammykingx.com.ng>
2022-11-05 09:02:48 +0000

    fixed pycodestyle error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,15 +4,10 @@ def search_replace(my_list, search, replace):
         A function that traverse through a list for an element that matches
         search and modify it with replace then returns a new list.
 
+        @elem: Elements
     '''
     if len(my_list) == 0:
         return my_list
 
-    new_lst = [element if element != search else replace for element in my_list]
+    new_lst = [elem if elem != search else replace for elem in my_list]
     return new_lst
-'''
-# Test
-my_lst = [1, 2, 3, 4, 5, 7, 8]
-print(my_lst)
-print(search_replace(my_lst, 2, 89))
-'''
