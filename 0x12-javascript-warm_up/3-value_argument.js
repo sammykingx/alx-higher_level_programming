c45c7aef9a099ad826f529de547381e43a244d50
sammy <sammy@sammykingx.com.ng>
2022-12-01 22:49:54 +0000

    print first argument to stdout in js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+   A js script that only prints the first argument passed to it on the
+   Terminal.
+ 
+   Commandline arguments in js is made possible using the process.argv
+   which is an array that contains all arguments of the CLI.
+
+   process.argv[0] contains the absolute path to the node binary file
+
+   process.argv[1] contains the absolute file path to the .js file in
+   which the js engine runs.
+
+   so process.argv[2] ... contains users passed in arguments.
+
+   Author: Sammykingx
+*/
+const value = process.argv[2];
+if (value === undefined) {
+  console.log('No argument');
+} else {
+  console.log(value);
+}
