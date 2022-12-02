0067fea943afe680dfb327f82afbca8a3c11e45d
sammy <sammy@sammykingx.com.ng>
2022-12-02 14:22:26 +0000

    Task 5 to 7

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+   A JS Script that prints the elements of an array in a single line.
+
+   Author: sammykingx
+
+   Using for loop to get this done, however other loop can still work.
+
+*/
+const lang = ['c programming', 'python programming', 'Javasript'];
+for (const item of lang) {
+  console.log(item);
+}
