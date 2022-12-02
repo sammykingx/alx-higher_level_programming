61f83c4155fa2cad1de42ebf2d822134aa97a15b
sammy <sammy@sammykingx.com.ng>
2022-12-02 14:38:53 +0000

    removed the number constructor

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,8 +7,8 @@
           2. then using the Number constructor to change the data type.
 
 */
-const value = Number(process.argv[2]);
-if (isNaN(value) || value === undefined) {
+const value = process.argv[2];
+if (isNaN(value)) {
   console.log('Not a number');
 } else {
   console.log('My number: ', value | 0);
